5a35229b168740c44ee68555f941f2e4076a9b1c
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2023-04-15 01:41:57 +0530

    Push

---
+++ CODE/variable2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//char, int, float, double, bool
+	
+	char a='S';
+	int b=1;
+	float c=2.90;
+	double d=12.20;
+	bool e=true;
+
+	cout << a;
+
+}
